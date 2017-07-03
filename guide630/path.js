d8a9b2d6246fb0997db1b8dc0c72fb0613c12d59
zengjie <thezj@vip.qq.com>
2017-07-03 10:22:34 +0800

    path模块学习

--- guide630/path.js
+++ guide630/path.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const fs = require('fs')
+const _ = require('lodash')
 let log = (...parameter) => {
     console.log(...parameter)
 }
@@ -10,11 +11,23 @@ let log = (...parameter) => {
 //     log(d)
 // })
 
-let p = path.normalize('.//a//b//d//..//c/e//..//') + 'message.txt'
-p = path.join(__dirname, 'a', 'b', 'c') + '\\message.txt'
-p = path.resolve(__dirname, 'a', 'b', 'c') + '\\message.txt'
+// let p = path.normalize('.//a//b//d//..//c/e//..//') + 'message.txt'
+// p = path.join(__dirname, 'a', 'b', 'c') + '\\message.txt'
+// p = path.resolve(__dirname, 'a', 'b', 'c') + '\\message.txt'
+// p = path.resolve('a', 'b', 'c','message.txt')
+// log(p)
+// let file = fs.createReadStream(p, 'utf8')
+// file.on('data', d => {
+//     log(d)
+// })
+
+let p = path.relative('c:\\v1\v2\v3', 'c:\\v1\v2x\v3x')
+p = path.relative('c:/sdf.xtx', 'a/b/c/message.txt')
 log(p)
-let file = fs.createReadStream(p, 'utf8')
-file.on('data', d => {
-    log(d)
-})
+// p = path.dirname(p)
+// p = path.basename(p,'.txt')
+p = path.join(path.dirname(p), path.basename(p, '.txt') + path.extname(p))
+
+log(p)
+log(path.sep)
+log(path.delimiter)
